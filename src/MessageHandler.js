c344a9d56440460e8dc03d4805ae4acd8261d705
Frysuni <ibragimzinatullin@mail.ru>
2022-08-09 17:26:48 +0500

    any

--- src/MessageHandler.js
+++ src/MessageHandler.js
@@ -1,10 +1,12 @@
 const { Chatter } = require('./chat/chatter.js');
 const { authorizateMember } = require('./authorization/authorizateMember.js');
 const WebSocket = require('ws');
+const { getKey } = require('./crypter/keyGenerate.js');
 
 const AuthedMembers = {};
 
 async function MessageHandler(message, clients) {
+    const key = await getKey();
     if (message.startsWith('message ')) {
         const AuthRecive = JSON.parse(message.slice(8));
         if (AuthedMembers[AuthRecive.author] != 'auth') {
@@ -17,6 +19,11 @@ async function MessageHandler(message, clients) {
 
         if (AuthResponce.status == true) {
             AuthedMembers[AuthResponce.username] = 'auth';
+            const map = {
+                key,
+                AuthedMembers: Object.keys(AuthedMembers)
+            };
+            return `map ${JSON.stringify(map)}`;
         }
 
         clients.forEach(function each(client) {
@@ -24,7 +31,6 @@ async function MessageHandler(message, clients) {
                 client.send('auth ' + JSON.stringify(AuthResponce));
             }
         });
-        console.log(AuthedMembers);
     }
     else if (message.startsWith('disconnect ')) {
         const AuthResponce = await authorizateMember(JSON.parse(message.slice(11)));
@@ -40,8 +46,8 @@ async function MessageHandler(message, clients) {
                 client.send('disconnect ' + JSON.stringify(AuthResponce));
             }
         });
-        console.log(AuthedMembers);
     }
+    else { console.log('undefined prefix'); }
 }
 
 module.exports = { MessageHandler };
